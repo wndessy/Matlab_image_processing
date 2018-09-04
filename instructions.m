556261ba97978d12c46ba43ba1a039ce855c39de
wndessy <wndessy@gmail.com>
2018-09-04 12:25:01 +0200

    first version the tasks done

---
+++ instructions.m
@@ -0,0 +1,13 @@
+a=10;
+b=sin(10);
+x=[0;2;4;6];
+y=[2 2 2 2; 5 5 5 5; 3 3 3 3 ;9 9 9 9];
+ansy=y+y;
+z=y.^2;
+m=y./2;
+identity=eye(3);
+lenth=length(x);
+one=ones(2);
+rand=rand(4);
+generated=[2:4:20];
+
---
+++ problem4.m
@@ -0,0 +1,34 @@
+% input matrix
+
+fprintf('4X4 matrix input')
+ 
+F=[1 2 3 4;5 6 7 8;9 10 11 12;13 14 15 16];
+ F
+ 
+ % a) Use the Matlab operation f = F(:). What is the resulting f?
+ fprintf('Using --- operator to transform matrix to 1D vector')
+ f=F(:);
+ f
+ % b) Use the Matlab function reshape to reconstruct the image matrix
+ fprintf('Reshaping the 1D vector back to a 4 by 4 matrix')
+ r1=reshape(f,4,4);
+ r1
+ 
+ %c) Does the colon-operator, (:), work for any image size, M × N, if N 6 not= M
+ fprintf('testing the --- operator with a 4X3 matrix')
+ F1=[1 2 3 4;5 6 7 8;9 10 11 12];
+ F1
+ f1=F1(:);
+ f1
+ fprintf('YES it works with a 3X4 matrix')
+
+ %d) Check how the following operations work: fr2 = F(2,:), and fc3 =
+ %F(:,3)
+ fprintf('find the second row of a given matrix F')
+ fr2 = F(2,:);
+ fr2
+ fprintf('select the third column of a given matrix F')
+ fc3=F(:,3);
+ fc3
+ 
+ 
